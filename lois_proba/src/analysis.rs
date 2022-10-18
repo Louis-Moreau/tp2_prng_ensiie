6ba76d96f7d56c30630219e21b0d85645551bb0a
Louis Moreau <elixar05@gmail.com>
2022-10-19 01:47:46 +0200

    latest

--- lois_proba/src/analysis.rs
+++ lois_proba/src/analysis.rs
@@ -1,21 +1,21 @@
 use plotters::prelude::*;
+use std::f64::consts::PI;
+use statrs::distribution::*;
 
-const SCATTER_SAMPLE : usize = 60000;
-const HISTO_SAMPLE : usize = 100;
+const SCATTER_SAMPLE: usize = 60000;
+const HISTO_SAMPLE: usize = 100;
 
-pub fn analyse(data : &Vec<f64>,yscale : f64, name : &str) -> Result<(), Box<dyn std::error::Error>> {
-    scatter_plot(data,name);
-    qq_plot(data,name);
-    histogram_plot(data,yscale,name);
+pub fn analyse(data: &Vec<f64>, yscale: f64, name: &str) -> Result<(), Box<dyn std::error::Error>> {
+    scatter_plot(data, name);
+    qq_plot(data, name);
+    histogram_plot(data, yscale, name);
 
     return Ok(());
 }
 
-
-pub fn scatter_plot(data : &Vec<f64> ,name : &str) -> Result<(), Box<dyn std::error::Error>>{
-
-    let mut max : f64 = 0f64;
-    let mut min : f64 = 0f64;
+pub fn scatter_plot(data: &Vec<f64>, name: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let mut max: f64 = 0f64;
+    let mut min: f64 = 0f64;
     for i in data {
         if *i > max {
             max = *i;
@@ -25,10 +25,14 @@ pub fn scatter_plot(data : &Vec<f64> ,name : &str) -> Result<(), Box<dyn std::er
         }
     }
 
-    let scatter_data : Vec<(f64,f64)> = data[0..SCATTER_SAMPLE].iter().enumerate().map(|(x)|return ((x.0 as f64) / SCATTER_SAMPLE as f64 , *x.1) ).collect();
+    let scatter_data: Vec<(f64, f64)> = data[0..SCATTER_SAMPLE]
+        .iter()
+        .enumerate()
+        .map(|(x)| return ((x.0 as f64) / SCATTER_SAMPLE as f64, *x.1))
+        .collect();
 
-    let path = format!("plots/scatter_{}.png",name);
-    let title = format!("Scatter {}, {} points",name,SCATTER_SAMPLE);
+    let path = format!("plots/scatter_{}.png", name);
+    let title = format!("Scatter {}, {} points", name, SCATTER_SAMPLE);
 
     let root = BitMapBackend::new(&path, (1920, 1080)).into_drawing_area();
     root.fill(&WHITE)?;
@@ -47,23 +51,34 @@ pub fn scatter_plot(data : &Vec<f64> ,name : &str) -> Result<(), Box<dyn std::er
         .draw()?;
 
     scatter_ctx.draw_series(
-        scatter_data.iter()
-            .map(|(x ,y)| Circle::new((*x,*y ), 1, BLUE.filled())),
+        scatter_data
+            .iter()
+            .map(|(x, y)| Circle::new((*x, *y), 1, BLUE.filled())),
     )?;
 
     // To avoid the IO failure being ignored silently, we manually call the present function
     root.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
 
-
-    return Ok(())
-
+    return Ok(());
 }
 
 //TODO add x = x line
-pub fn qq_plot(data : &Vec<f64>, name: &str)-> Result<(), Box<dyn std::error::Error>> {
-    let mut max : f64 = 0f64;
-    let mut min : f64 = 0f64;
-    for i in data {
+pub fn qq_plot(data: &Vec<f64>, name: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let mut max: f64 = 0f64;
+    let mut min: f64 = 0f64;
+
+    let sample_size = data.len() as usize;
+    let mut qq_data = data.clone();
+
+    let dev = std_deviation(&qq_data).unwrap();
+    let mean = mean(&qq_data).unwrap();
+    let mut normal = Normal::new(mean, dev).unwrap();
+    
+    for i in 0..sample_size {
+        qq_data[i] =  normal.cdf(qq_data[i]) ;
+    }
+
+    for i in &qq_data {
         if *i > max {
             max = *i;
         }
@@ -72,14 +87,15 @@ pub fn qq_plot(data : &Vec<f64>, name: &str)-> Result<(), Box<dyn std::error::Er
         }
     }
 
-
-    let sample_size = data.len() as u32;
-    let mut qq_data = data.clone();
     qq_data.sort_by(|a, b| a.partial_cmp(b).unwrap());
-    let qq_data : Vec<(f64,f64)> = qq_data.iter().enumerate().map(|(x)|return (x.0 as f64 /  sample_size as f64 , *x.1)).collect();
-
-    let path = format!("plots/qqplot_{}.png",name);
-    let title = format!("QQ Plot {}",name);
+    let qq_data: Vec<(f64, f64)> = qq_data
+        .iter()
+        .enumerate()
+        .map(|(x)| return (x.0 as f64 / sample_size as f64, *x.1))
+        .collect();
+        
+    let path = format!("plots/qqplot_{}.png", name);
+    let title = format!("QQ Plot {}", name);
 
     let root = BitMapBackend::new(&path, (1920, 1080)).into_drawing_area();
     root.fill(&WHITE)?;
@@ -98,9 +114,16 @@ pub fn qq_plot(data : &Vec<f64>, name: &str)-> Result<(), Box<dyn std::error::Er
         .draw()?;
 
     scatter_ctx.draw_series(
-        qq_data.iter()
-            .map(|(x ,y)| Circle::new((*x,*y ), 1, BLUE.filled())),
+        qq_data
+            .iter()
+            .map(|(x, y)| Circle::new((*x, *y), 3, BLUE.filled())),
     )?;
+
+    scatter_ctx.draw_series(
+        LineSeries::new(
+        (0..2).map(|x| (x as f64, x as f64)),
+        ShapeStyle { color: RGBAColor(255, 0, 0, 255f64), filled: true, stroke_width: 2 },
+    ))?;
 
     // To avoid the IO failure being ignored silently, we manually call the present function
     root.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
@@ -108,10 +131,13 @@ pub fn qq_plot(data : &Vec<f64>, name: &str)-> Result<(), Box<dyn std::error::Er
     return Ok(());
 }
 
-pub fn histogram_plot(data : &Vec<f64>, yscale : f64, name: &str)-> Result<(), Box<dyn std::error::Error>> {
-
-    let mut max : f64 = 0f64;
-    let mut min : f64 = 0f64;
+pub fn histogram_plot(
+    data: &Vec<f64>,
+    yscale: f64,
+    name: &str,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut max: f64 = 0f64;
+    let mut min: f64 = 0f64;
     for i in data {
         if *i > max {
             max = *i;
@@ -123,8 +149,8 @@ pub fn histogram_plot(data : &Vec<f64>, yscale : f64, name: &str)-> Result<(), B
 
     let sample_size = data.len() as u32;
 
-    let path = format!("plots/histogramme_{}.png",name);
-    let title = format!("Histogramme {}",name);
+    let path = format!("plots/histogramme_{}.png", name);
+    let title = format!("Histogramme {}", name);
 
     let root = BitMapBackend::new(&path, (1920, 1080)).into_drawing_area();
     root.fill(&WHITE)?;
@@ -134,27 +160,61 @@ pub fn histogram_plot(data : &Vec<f64>, yscale : f64, name: &str)-> Result<(), B
         .y_label_area_size(50)
         .margin(30)
         .caption(&title, ("sans-serif", 100.0))
-        .build_cartesian_2d((min..max).step((max-min)/HISTO_SAMPLE as f64).use_floor(), 0f64..1f64)?;
-        histogram_ctx
+        .build_cartesian_2d(
+            (min..max)
+                .step((max - min) / HISTO_SAMPLE as f64)
+                .use_floor(),
+            0f64..1f64,
+        )?;
+    histogram_ctx
         .configure_mesh()
         .disable_x_mesh()
         .disable_y_mesh()
         .label_style(("sans-serif", 25.0))
         .draw()?;
 
-        histogram_ctx.draw_series(
-            Histogram::vertical(&histogram_ctx)
+    histogram_ctx.draw_series(
+        Histogram::vertical(&histogram_ctx)
             .style(BLUE.stroke_width(3))
             .margin(0)
-            .data(data.iter().map(|x| (*x, 1f64/sample_size as f64 * HISTO_SAMPLE as f64 * yscale))),
-        )?;
+            .data(
+                data.iter()
+                    .map(|x| (*x, 1f64 / sample_size as f64 * HISTO_SAMPLE as f64 * yscale)),
+            ),
+    )?;
 
-        // To avoid the IO failure being ignored silently, we manually call the present function
+    // To avoid the IO failure being ignored silently, we manually call the present function
     root.present().expect("Unable to write result to file, please make sure 'plotters-doc-data' dir exists under current dir");
 
     return Ok(());
 }
 
+
+
+fn mean(data: &Vec<f64>) -> Option<f64> {
+    let sum = data.iter().sum::<f64>() as f64;
+    let count = data.len();
+
+    match count {
+        positive if positive > 0 => Some(sum / count as f64),
+        _ => None,
+    }
+}
+
+fn std_deviation(data: &Vec<f64>) -> Option<f64> {
+    match (mean(data), data.len()) {
+        (Some(data_mean), count) if count > 0 => {
+            let variance = data.iter().map(|value| {
+                let diff = data_mean - (*value as f64);
+
+                diff * diff
+            }).sum::<f64>() / count as f64;
+
+            Some(variance.sqrt())
+        },
+        _ => None
+    }
+}
 /*
 Khi2
 Moyenne
@@ -169,4 +229,3 @@ scatter plot DONE
 
 
 */
-
--- lois_proba/src/main.rs
+++ lois_proba/src/main.rs
@@ -1,46 +1,132 @@
 mod analysis;
 
-use rand_xoshiro::rand_core::{SeedableRng,RngCore};
+use crate::analysis::*;
+use rand_xoshiro::rand_core::{RngCore, SeedableRng};
 use rand_xoshiro::Xoshiro256PlusPlus;
 use statrs::function::erf::*;
-use crate::analysis::*;
-use std::time::{Instant, Duration};
+use std::f64::consts::PI;
+use std::time::{Duration, Instant};
 
-const BENCHMARK_SIZE : usize = 1000_0000;
+const BENCHMARK_SIZE: usize = 1_000_000;
 
-const SEED : u64 = 555 ;
+const SEED: u64 = 555;
 
 fn main() {
     println!("Hello, world!");
 
     let mut xoshiro = Xoshiro256PlusPlus::seed_from_u64(SEED);
 
-/* 
-    let mut vec_loi = vec![0f64;BENCHMARK_SIZE];
+    let mut vec_loi = vec![0f64; BENCHMARK_SIZE];
+    let now = Instant::now();
     for i in 0..BENCHMARK_SIZE {
-        vec_loi[i] =  f64::sqrt(2f64)* erf_inv(2f64*normalize(xoshiro.next_u32())- 1f64);
+        vec_loi[i] = f64::sqrt(2f64) * erf_inv(2f64 * normalize(xoshiro.next_u64()) - 1f64);
     }
-    analyse(&vec_loi,0.2f64,"Loi Inverse");*/
+    print_info(&vec_loi,now.elapsed(),"Methode Inverse");
+    analyse(&vec_loi, 0.2f64, "Methode Inverse");
 
 
-    let mut vec_centrale_limite = vec![0f64;BENCHMARK_SIZE];
-    let n = 1000;
+    let mut vec_centrale_limite = vec![0f64; BENCHMARK_SIZE];
+    let n = 100;
     let now = Instant::now();
     for i in 0..BENCHMARK_SIZE {
         let mut somme = 0f64;
         for _j in 0..n {
-            somme += normalize(xoshiro.next_u32());
+            somme += normalize(xoshiro.next_u64());
         }
-        vec_centrale_limite[i] =  (somme - n as f64/2f64) / f64::sqrt(n as f64/12f64)
+        vec_centrale_limite[i] = (somme - n as f64 / 2f64) / f64::sqrt(n as f64 / 12f64)
     }
-    print!("temps : {:?}",now.elapsed());
-    analyse(&vec_centrale_limite,0.2f64,"Loi Centrale Limite");
+    print_info(&vec_centrale_limite,now.elapsed(),"Loi Centrale Limite");
+    analyse(&vec_centrale_limite, 0.2f64, "Loi Centrale Limite");
+
+    let mut vec_box_muller = vec![0f64; BENCHMARK_SIZE];
+    let now = Instant::now();
+    for i in 0..(BENCHMARK_SIZE / 2) {
+        let x = normalize(xoshiro.next_u64());
+        let y = normalize(xoshiro.next_u64());
+
+        vec_box_muller[2 * i] = f64::sqrt(-2f64 * f64::ln(x)) * f64::cos(2f64 * PI * y);
+        vec_box_muller[2 * i + 1] = f64::sqrt(-2f64 * f64::ln(x)) * f64::sin(2f64 * PI * y);
+    }
+    print_info(&vec_box_muller,now.elapsed(),"Methode de Box et Muller");
+    analyse(&vec_box_muller, 0.2f64, "Methode de Box et Muller");
+
+    let mut vec_marsaglia = vec![0f64; BENCHMARK_SIZE];
+    let now = Instant::now();
+    for i in 0..(BENCHMARK_SIZE / 2) {
+        let mut x: f64;
+        let mut y: f64;
+        let mut s: f64;
+        loop {
+            x = (normalize(xoshiro.next_u64()) - 0.5f64) * 2f64;
+            y = (normalize(xoshiro.next_u64()) - 0.5f64) * 2f64;
+            s = x * x + y * y;
+            if s > 0f64 && s < 1f64 {
+                break;
+            }
+        }
+        vec_marsaglia[2 * i] = x * (f64::sqrt((-2f64 * f64::ln(s)) / s));
+        vec_marsaglia[2 * i + 1] = y * (f64::sqrt((-2f64 * f64::ln(s)) / s));
+    }
+    print_info(&vec_marsaglia,now.elapsed(),"Methode de Marsaglia");
+    analyse(&vec_marsaglia, 0.2f64, "Methode de Marsaglia");
 
 
+    let mut vec_laplace = vec![1f64; BENCHMARK_SIZE];
+    let now = Instant::now();
+    for i in 0..BENCHMARK_SIZE {
+        loop {
+            let a = normalize(xoshiro.next_u64());
+            let u = normalize(xoshiro.next_u64());
+            let y = f64::signum(a - 0.5f64)*f64::ln(1f64-2f64*f64::abs(a- 0.5f64));
+            let f = f64::exp(-(y*y)/2f64)/ f64::sqrt(2f64 * PI);
+            let g = 0.5f64* f64::exp(- f64::abs(y));
+            let c = 2f64*f64::sqrt(f64::exp(1f64)/(2f64 * PI));
 
+            if c * g * u <= f {
+                if(y == 0f64) {
+                    println!("zero");
+                }
+                vec_laplace[i] = y;
+                break;
+            }
+        }
+    }
+    print_info(&vec_laplace,now.elapsed(),"Methode du rejet - Laplace");
+    analyse(&vec_laplace, 0.2f64, "Methode du rejet - Laplace");
 
 }
 
-fn normalize(input : u32) -> f64 {
-    return input as f64 / u32::MAX as f64;
+fn normalize(input: u64) -> f64 {
+    return input as f64 / u64::MAX as f64;
+}
+
+fn mean(data: &Vec<f64>) -> Option<f64> {
+    let sum = data.iter().sum::<f64>() as f64;
+    let count = data.len();
+
+    match count {
+        positive if positive > 0 => Some(sum / count as f64),
+        _ => None,
+    }
+}
+
+fn std_deviation(data: &Vec<f64>) -> Option<f64> {
+    match (mean(data), data.len()) {
+        (Some(data_mean), count) if count > 0 => {
+            let variance = data.iter().map(|value| {
+                let diff = data_mean - (*value as f64);
+
+                diff * diff
+            }).sum::<f64>() / count as f64;
+
+            Some(variance.sqrt())
+        },
+        _ => None
+    }
+}
+
+fn print_info(vec : &Vec<f64>,dur :Duration,nom : &str) {
+    let dev = std_deviation(vec).unwrap();
+    let mean = mean(vec).unwrap();
+    println!("{} : Esperance: {} , Ecart-type: {} , Temps de calcul: {:?} : {:?} par iter ",nom,mean,dev,dur,dur/BENCHMARK_SIZE as u32)
 }
